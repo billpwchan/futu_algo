a6502087365b98e95cd3766d78ad1b7131d3f7f1
Bill Chan <billpwchan@hotmail.com>
2021-01-04 03:34:39 +0000

    Update Bid_1 Logic. Remove the need to use Bid1 Price
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- main.py
+++ main.py
@@ -40,7 +40,13 @@ def main():
     # futu_trade.store_all_data_database()
 
     # Initialize Strategies
-    stock_list = ['HK.09988', 'HK.01211']
+    stock_list = ["HK.00001", "HK.00002", "HK.00003", "HK.00005", "HK.00006", "HK.00011", "HK.00012", "HK.00016",
+                  "HK.00017", "HK.00027", "HK.00066", "HK.00101", "HK.00175", "HK.00267", "HK.00288", "HK.00386",
+                  "HK.00388", "HK.00669", "HK.00688", "HK.00700", "HK.00762", "HK.00823", "HK.00857", "HK.00883",
+                  "HK.00939", "HK.00941", "HK.01038", "HK.01044", "HK.01093", "HK.01109", "HK.01113", "HK.01177",
+                  "HK.01299", "HK.01398", "HK.01810", "HK.01876", "HK.01928", "HK.01997", "HK.02007", "HK.02018",
+                  "HK.02020", "HK.02269", "HK.02313", "HK.02318", "HK.02319", "HK.02382", "HK.02388", "HK.02628",
+                  "HK.03328", "HK.03690", "HK.03988", "HK.09988"]
     input_data = futu_trade.get_1M_data(stock_list=stock_list)
     macd_cross = MACDCross(input_data=input_data)
     futu_trade.stock_price_subscription(input_data, stock_list=stock_list, strategy=macd_cross, timeout=100000)
