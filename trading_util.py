5cbe3f48991dd7f93a5e523cf1454dbb2893209a
Bill Chan <billpwchan@hotmail.com>
2021-01-10 05:49:40 +0000

    Update Withdrawal Buy Order logic
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- trading_util.py
+++ trading_util.py
@@ -27,10 +27,41 @@ class TradingUtil:
             self.default_logger.error(f"Cannot acquire account position {position_data}")
             return
             # raise Exception('账户信息获取失败: {}'.format(position_data))
+
+        # Already Holds Position of the stock. No need to buy another share
         if not position_data.empty:
             self.default_logger.warn(f"Account holds position for stock {stock_code}")
             return
 
+        # Check if an order has already been made but not filled_completely
+        ret_code, order_list_data = self.trade_ctx.order_list_query(order_id="",
+                                                                    status_filter_list=self.status_filter_list,
+                                                                    code=stock_code, start='', end='',
+                                                                    trd_env=self.trd_env,
+                                                                    acc_id=0, acc_index=0, refresh_cache=False)
+        if ret_code != RET_OK:
+            self.default_logger.error(f"Cannot acquire order list {order_list_data}")
+            return
+            # raise Exception('今日订单列表获取异常 {}'.format(market_data))
+
+        # If Unfilled BUY ORDER is detected, no need to issue another BUY ORDER
+        if not order_list_data.empty and any(
+                record == TrdSide.BUY for record in order_list_data['trd_side'].tolist()):
+            self.default_logger.info(
+                f"Order already sent but not filled yet for {stock_code} with details \n {order_list_data}")
+            return
+
+        # Withdrawal unfilled sell order if a new BUY DECISION is made
+        if not order_list_data.empty and any(
+                record == TrdSide.SELL for record in order_list_data['trd_side'].tolist()):
+            self.default_logger.info(
+                f"Detected unfilled SELL ORDER for {stock_code}, withdrawal based on new Buy Decision")
+            # Iterate all unfilled Sell order
+            for index, row in order_list_data['order_id'].iterrows():
+                self.trade_ctx.modify_order(ModifyOrderOp.CANCEL, row['order_id'], 0, 0)
+            return
+
+        # Use Current Market Price / Bid-1 Buy Price to place a buy order
         ret_code, market_data = self.quote_ctx.get_market_snapshot([stock_code])
         if ret_code != RET_OK:
             self.default_logger.error(f"Cannot acquire market snapshot {market_data}")
@@ -44,31 +75,9 @@ class TradingUtil:
             self.default_logger.error("can't get orderbook, retrying:{}".format(order_data))
             return
             # raise Exception('摆盘数据获取异常 {}'.format(order_data))
-
         cur_price = order_data['Bid'][0][0]  # 取得买一价
 
-        # Check if an order has already been made but not filled_completely
-        ret_code, order_list_data = self.trade_ctx.order_list_query(order_id="",
-                                                                    status_filter_list=self.status_filter_list,
-                                                                    code=stock_code, start='', end='',
-                                                                    trd_env=self.trd_env,
-                                                                    acc_id=0, acc_index=0, refresh_cache=False)
-        if ret_code != RET_OK:
-            self.default_logger.error(f"Cannot acquire order list {order_list_data}")
-            return
-            # raise Exception('今日订单列表获取异常 {}'.format(market_data))
-        if not order_list_data.empty and any(record == TrdSide.BUY for record in order_list_data['trd_side'].tolist()):
-            self.default_logger.info(
-                f"Order already sent but not filled yet for {stock_code} with details \n {order_list_data}")
-            return
-        if not order_list_data.empty and any(record == TrdSide.SELL for record in order_list_data['trd_side'].tolist()):
-            self.default_logger.info(
-                f"Detected unfilled sell order for {stock_code}, withdrawal based on new Buy Decision")
-            # Iterate all unfilled Sell order
-            for index, row in order_list_data['order_id'].iterrows():
-                self.trade_ctx.modify_order(ModifyOrderOp.CANCEL, row['order_id'], 0, 0)
-
-        # Place Buy Order with Current Price & 1 lot_size
+        # Placing Order
         for i in range(2):
             ret_code, ret_data = self.trade_ctx.place_order(
                 price=cur_price,
@@ -93,8 +102,38 @@ class TradingUtil:
             self.default_logger.error(f"Cannot acquire account position {position_data}")
             return
             # raise Exception('账户信息获取失败: {}'.format(position_data))
+
+        # Already Holds No Position of the stock. No need to SELL
         if position_data.empty:
             self.default_logger.warn(f"Account does not hold any position for stock {stock_code}")
+            return
+
+        # Check if an order has already been made but not filled_completely
+        ret_code, order_list_data = self.trade_ctx.order_list_query(order_id="",
+                                                                    status_filter_list=self.status_filter_list,
+                                                                    code=stock_code, start='', end='',
+                                                                    trd_env=self.trd_env,
+                                                                    acc_id=0, acc_index=0, refresh_cache=False)
+        if ret_code != RET_OK:
+            self.default_logger.error(f"Cannot acquire order list {order_list_data}")
+            return
+            # raise Exception('今日订单列表获取异常 {}'.format(market_data))
+
+        # If Unfilled SELL ORDER is detected, no need to issue another SELL ORDER
+        if not order_list_data.empty and any(
+                record == TrdSide.SELL for record in order_list_data['trd_side'].tolist()):
+            self.default_logger.info(
+                f"Order already sent but not filled yet for {stock_code} with details \n {order_list_data}")
+            return
+
+        # Withdrawal unfilled Buy ORDER if a new SELL DECISION is made
+        if not order_list_data.empty and any(
+                record == TrdSide.BUY for record in order_list_data['trd_side'].tolist()):
+            self.default_logger.info(
+                f"Detected unfilled BUY ORDER for {stock_code}, withdrawal based on new SELL Decision")
+            # Iterate all unfilled Buy order
+            for index, row in order_list_data['order_id'].iterrows():
+                self.trade_ctx.modify_order(ModifyOrderOp.CANCEL, row['order_id'], 0, 0)
             return
 
         position_data = position_data.set_index('code')
@@ -118,22 +157,6 @@ class TradingUtil:
                 self.default_logger.error("can't get orderbook, retrying:{}".format(order_data))
 
             cur_price = order_data['Bid'][0][0]  # 取得买一价
-
-            # Check if an order has already been made but not filled_completely
-            ret_code, order_list_data = self.trade_ctx.order_list_query(order_id="",
-                                                                        status_filter_list=self.status_filter_list,
-                                                                        code=stock_code, start='', end='',
-                                                                        trd_env=self.trd_env,
-                                                                        acc_id=0, acc_index=0, refresh_cache=False)
-            if ret_code != RET_OK:
-                self.default_logger.error(f"Cannot acquire order list {order_list_data}")
-                return
-                # raise Exception('今日订单列表获取异常 {}'.format(market_data))
-            if not order_list_data.empty and all(
-                    record == TrdSide.SELL for record in order_list_data['trd_side'].tolist()):
-                self.default_logger.info(
-                    f"Order already sent but not filled yet for {stock_code} with details \n {order_list_data}")
-                return
 
             # Place Sell Order with current price and 1 lot size
             for i in range(2):
