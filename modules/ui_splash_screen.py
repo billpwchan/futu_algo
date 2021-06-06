eced3bb0b1a8e63484cb86c2bd4f816d3768d999
Bill Chan <billpwchan@hotmail.com>
2021-06-06 12:05:16 +0100

    Update Copyright Profile
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- modules/ui_splash_screen.py
+++ modules/ui_splash_screen.py
@@ -1,12 +1,38 @@
 # -*- coding: utf-8 -*-
 
 #  Futu Algo: Algorithmic High-Frequency Trading Framework
-#  Copyright (c)  billpwchan - All Rights Reserved
-#  Unauthorized copying of this file, via any medium is strictly prohibited
-#  Proprietary and confidential
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
 #  Written by Bill Chan <billpwchan@hotmail.com>, 2021
+#  Copyright (c)  billpwchan - All Rights Reserved
 
-################################################################################
+#  Futu Algo: Algorithmic High-Frequency Trading Framework
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  Written by Bill Chan <billpwchan@hotmail.com>, 2021
+#  C############################################################################
 ## Form generated from reading UI file 'splash_screenWmiKOh.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.0.3
